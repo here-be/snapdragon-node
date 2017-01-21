5d4c07e483d42a0c1da2659857dc20315a9e4ad3
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-21 04:51:48 -0500

    adds methods and tests for getting/setting nodes

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 
+var utils = require('snapdragon-util');
 var define = require('define-property');
 var isObject = require('isobject');
+var getters = ['siblings', 'index', 'first', 'lext', 'prev', 'next'];
+
+/**
+ * Create a new AST `Node` with the given `val` and `type`.
+ *
+ * ```js
+ * var node = new Node('*', 'Star');
+ * var node = new Node({type: 'star', val: '*'});
+ * ```
+ * @name Node
+ * @param {String|Object} `val` Pass a matched substring, or an object to merge onto the node.
+ * @param {String} `type` The node type to use when `val` is a string.
+ * @return {Object} node instance
+ * @api public
+ */
 
 class Node {
   constructor(val, type) {
     this.define('isNode', true);
     if (isObject(val)) {
       for (var key in val) {
-        this[key] = val[key];
+        if (getters.indexOf(key) === -1) {
+          this[key] = val[key];
+        }
       }
     } else {
       this.type = type;
@@ -16,25 +34,282 @@ class Node {
     }
   }
 
+  /**
+   * Define a non-enumberable property on the node instance.
+   *
+   * ```js
+   * var node = new Node();
+   * node.define('foo', 'something non-enumerable');
+   * ```
+   * @name .define
+   * @param {String} `name`
+   * @param {any} `val`
+   * @return {Object} returns the node instance
+   * @api public
+   */
+
   define(name, val) {
     define(this, name, val);
     return this;
   }
 
-  addNode(node) {
+  /**
+   * Given node `foo` and node `bar`, push node `bar` onto `foo.nodes`, and
+   * set `foo` as `bar.parent`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * foo.pushNode(bar);
+   * ```
+   * @name .pushNode
+   * @param {Object} `node`
+   * @return {undefined}
+   * @api public
+   */
+
+  pushNode(node) {
     this.nodes = this.nodes || [];
-    node.define('index', this.nodes.length);
-    node.define('parent', this);
-    node.define('siblings', this.nodes);
+    define(node, 'parent', this);
     this.nodes.push(node);
   }
 
-  prev(n) {
-    return this.siblings[this.index - (n || 1)] || this.parent.prev(n);
+  /**
+   * Alias for [pushNode](#pushNode) for backwards compatibility with 0.1.0.
+   * @name .addNode
+   * @api public
+   */
+
+  addNode(node) {
+    return this.pushNode(node);
   }
 
-  next(n) {
-    return this.siblings[this.index + (n || 1)] || this.parent.next(n);
+  /**
+   * Given node `foo` and node `bar`, unshift node `bar` onto `foo.nodes`, and
+   * set `foo` as `bar.parent`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * foo.unshiftNode(bar);
+   * ```
+   * @name .unshiftNode
+   * @param {Object} `node`
+   * @return {undefined}
+   * @api public
+   */
+
+  unshiftNode(node) {
+    this.nodes = this.nodes || [];
+    define(node, 'parent', this);
+    this.nodes.unshift(node);
+  }
+
+  /**
+   * Get the first child node from `node.nodes` that matches the given `type`.
+   * If `type` is a number, the child node at that index is returned.
+   *
+   * ```js
+   * var child = node.getNode(1); //<= index of the node to get
+   * var child = node.getNode('foo');
+   * var child = node.getNode(/^(foo|bar)$/);
+   * var child = node.getNode(['foo', 'bar']);
+   * ```
+   * @name .getNode
+   * @param {String} `type`
+   * @return {Object} Returns a child node or undefined.
+   * @api public
+   */
+
+  getNode(type) {
+    return utils.getNode(this.nodes, type);
+  }
+
+  /**
+   * Return true if the node is the given `type`.
+   *
+   * ```js
+   * var node = new Node({type: 'bar'});
+   * cosole.log(node.isType('foo'));          // false
+   * cosole.log(node.isType(/^(foo|bar)$/));  // true
+   * cosole.log(node.isType(['foo', 'bar'])); // true
+   * ```
+   * @name .isType
+   * @param {String} `type`
+   * @return {Boolean}
+   * @api public
+   */
+
+  isType(type) {
+    return utils.isType(this, type);
+  }
+
+  /**
+   * Return true if the `node.nodes` has the given `type`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * foo.pushNode(bar);
+   *
+   * cosole.log(foo.hasType('qux'));          // false
+   * cosole.log(foo.hasType(/^(qux|bar)$/));  // true
+   * cosole.log(foo.hasType(['qux', 'bar'])); // true
+   * ```
+   * @name .hasType
+   * @param {String} `type`
+   * @return {Boolean}
+   * @api public
+   */
+
+  hasType(type) {
+    return utils.hasType(this, type);
+  }
+
+  /**
+   * Get the siblings array, or `null` if it doesn't exist.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   *
+   * console.log(bar.siblings.length) // 2
+   * console.log(baz.siblings.length) // 2
+   * ```
+   * @name .siblings
+   * @return {Array}
+   * @api public
+   */
+
+  get siblings() {
+    return this.parent ? this.parent.nodes : null;
+  }
+
+  /**
+   * Get the previous node from the siblings array or `null`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   *
+   * console.log(baz.prev.type) // 'bar'
+   * ```
+   * @name .prev
+   * @return {Object}
+   * @api public
+   */
+
+  get prev() {
+    return this.parent && this.siblings
+      ? this.siblings[this.index - 1] || this.parent.prev
+      : null;
+  }
+
+  /**
+   * Get the siblings array, or `null` if it doesn't exist.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   *
+   * console.log(bar.siblings.length) // 2
+   * console.log(baz.siblings.length) // 2
+   * ```
+   * @name .next
+   * @return {Object}
+   * @api public
+   */
+
+  get next() {
+    return this.parent && this.siblings
+      ? this.siblings[this.index + 1] || this.parent.next
+      : null;
+  }
+
+  /**
+   * Get the node's current index from `node.parent.nodes`.
+   * This should always be correct, even when the parent adds nodes.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * var qux = new Node({type: 'qux'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   * foo.unshiftNode(qux);
+   *
+   * console.log(bar.index) // 1
+   * console.log(baz.index) // 2
+   * console.log(qux.index) // 0
+   * ```
+   * @name .index
+   * @return {Number}
+   * @api public
+   */
+
+  get index() {
+    if (!this.siblings) return -1;
+    for (var i = 0; i < this.siblings.length; i++) {
+      if (this === this.siblings[i]) {
+        return i;
+      }
+    }
+  }
+
+  /**
+   * Get the first node from `node.nodes`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * var qux = new Node({type: 'qux'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   * foo.pushNode(qux);
+   *
+   * console.log(foo.first.type) // 'bar'
+   * ```
+   * @name .first
+   * @return {Object} The first node, or undefiend
+   * @api public
+   */
+
+  get first() {
+    return utils.arrayify(this.nodes)[0];
+  }
+
+  /**
+   * Get the last node from `node.nodes`.
+   *
+   * ```js
+   * var foo = new Node({type: 'foo'});
+   * var bar = new Node({type: 'bar'});
+   * var baz = new Node({type: 'baz'});
+   * var qux = new Node({type: 'qux'});
+   * foo.pushNode(bar);
+   * foo.pushNode(baz);
+   * foo.pushNode(qux);
+   *
+   * console.log(foo.last.type) // 'qux'
+   * ```
+   * @name .last
+   * @return {Object} The last node, or undefiend
+   * @api public
+   */
+
+  get last() {
+    return utils.last(utils.arrayify(this.nodes));
   }
 };
 
--- test/test.js
+++ test/test.js
@@ -2,86 +2,265 @@
 
 require('mocha');
 var assert = require('assert');
+var captureSet = require('snapdragon-capture-set');
 var Parser = require('snapdragon/lib/parser');
 var Node = require('..');
 var parser;
+var ast;
 
 describe('snapdragon-node', function() {
   beforeEach(function() {
-    parser = new Parser();
+    parser = new Parser()
+    parser.use(captureSet())
+      .captureSet('brace', /^\{/, /^\}/)
+      .set('text', function() {
+        var pos = this.position();
+        var m = this.match(/^[^{}]/);
+        if (!m) return;
+        return pos({
+          type: 'text',
+          val: m[0]
+        });
+      });
+
+    // ensure the ast is an instance of Node
+    ast = new Node(parser.parse('{a{b}c}'));
   });
 
-  it('should export a function', function() {
-    assert.equal(typeof Node, 'function');
+  describe('node', function() {
+    it('should export a function', function() {
+      assert.equal(typeof Node, 'function');
+    });
+
+    it('should create a new Node with the given object', function() {
+      var node = new Node({val: '*', type: 'star'});
+      assert.equal(node.val, '*');
+      assert.equal(node.type, 'star');
+    });
+
+    it('should create a new Node with the given position', function() {
+      var pos = parser.position();
+      var node = pos(new Node());
+
+      assert(node.position);
+      assert(node.position.start);
+      assert(node.position.start.line);
+      assert(node.position.start.column);
+
+      assert(node.position.end);
+      assert(node.position.end.line);
+      assert(node.position.end.column);
+    });
+
+    it('should create a new Node with the given position and val', function() {
+      var pos = parser.position();
+      var node = pos(new Node('*'));
+
+      assert.equal(node.val, '*');
+
+      assert(node.position);
+      assert(node.position.start);
+      assert(node.position.start.line);
+      assert(node.position.start.column);
+
+      assert(node.position.end);
+      assert(node.position.end.line);
+      assert(node.position.end.column);
+    });
+
+    it('should create a new Node with the given position, type, and val', function() {
+      var pos = parser.position();
+      var node = pos(new Node('*', 'star'));
+
+      assert.equal(node.val, '*');
+      assert.equal(node.type, 'star');
+
+      assert(node.position);
+      assert(node.position.start);
+      assert(node.position.start.line);
+      assert(node.position.start.column);
+
+      assert(node.position.end);
+      assert(node.position.end.line);
+      assert(node.position.end.column);
+    });
+
+    it('should create a new Node with the given position and object', function() {
+      var pos = parser.position();
+      var node = pos(new Node({val: '*', type: 'star'}));
+
+      assert.equal(node.val, '*');
+      assert.equal(node.type, 'star');
+
+      assert(node.position);
+      assert(node.position.start);
+      assert(node.position.start.line);
+      assert(node.position.start.column);
+
+      assert(node.position.end);
+      assert(node.position.end.line);
+      assert(node.position.end.column);
+    });
   });
 
-  it('should create a new Node with the given object', function() {
-    var node = new Node({val: '*', type: 'star'});
-    assert.equal(node.val, '*');
-    assert.equal(node.type, 'star');
+  describe('.isType', function() {
+    it('should return true if the node is the given type', function() {
+      assert(ast.isType('root'));
+      assert(ast.last.isType('eos'));
+    });
   });
 
-  it('should create a new Node with the given position', function() {
-    var pos = parser.position();
-    var node = pos(new Node());
-
-    assert(node.position);
-    assert(node.position.start);
-    assert(node.position.start.line);
-    assert(node.position.start.column);
-
-    assert(node.position.end);
-    assert(node.position.end.line);
-    assert(node.position.end.column);
+  describe('.hasType', function() {
+    it('should return true if node.nodes has the given type', function() {
+      assert(ast.hasType('brace'));
+      assert(!ast.hasType('foo'));
+    });
   });
 
-  it('should create a new Node with the given position and val', function() {
-    var pos = parser.position();
-    var node = pos(new Node('*'));
-
-    assert.equal(node.val, '*');
-
-    assert(node.position);
-    assert(node.position.start);
-    assert(node.position.start.line);
-    assert(node.position.start.column);
-
-    assert(node.position.end);
-    assert(node.position.end.line);
-    assert(node.position.end.column);
+  describe('.first', function() {
+    it('should get the first node from `node.nodes`', function() {
+      assert(ast.first);
+      assert.equal(ast.first.type, 'bos');
+    });
   });
 
-  it('should create a new Node with the given position, type, and val', function() {
-    var pos = parser.position();
-    var node = pos(new Node('*', 'star'));
-
-    assert.equal(node.val, '*');
-    assert.equal(node.type, 'star');
-
-    assert(node.position);
-    assert(node.position.start);
-    assert(node.position.start.line);
-    assert(node.position.start.column);
-
-    assert(node.position.end);
-    assert(node.position.end.line);
-    assert(node.position.end.column);
+  describe('.last', function() {
+    it('should get the last node from `node.nodes`', function() {
+      assert(ast.last);
+      assert.equal(ast.last.type, 'eos');
+    });
   });
 
-  it('should create a new Node with the given position and object', function() {
-    var pos = parser.position();
-    var node = pos(new Node({val: '*', type: 'star'}));
+  describe('.index', function() {
+    it('should get the index of the node from `node.parent.nodes`', function() {
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      var qux = new Node({type: 'qux'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+      foo.unshiftNode(qux);
 
-    assert.equal(node.val, '*');
-    assert.equal(node.type, 'star');
+      assert.equal(bar.index, 1);
+      assert.equal(baz.index, 2);
+      assert.equal(qux.index, 0);
+    });
+  });
 
-    assert(node.position);
-    assert(node.position.start);
-    assert(node.position.start.line);
-    assert(node.position.start.column);
+  describe('.siblings', function() {
+    it('should get `node.parent.nodes`', function() {
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      var qux = new Node({type: 'qux'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+      foo.unshiftNode(qux);
 
-    assert(node.position.end);
-    assert(node.position.end.line);
-    assert(node.position.end.column);
+      assert.equal(foo.siblings, null);
+      assert.equal(bar.siblings.length, 3);
+      assert.equal(baz.siblings.length, 3);
+      assert.equal(qux.siblings.length, 3);
+    });
+  });
+
+  describe('.next', function() {
+    it('should get the next node from `node.nodes`', function() {
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+
+      assert.equal(bar.next.type, 'baz');
+      assert.equal(baz.next, null);
+    });
+
+    it('should get the next node from `node.parent.nodes`', function() {
+      var parent = new Node({type: 'parent'});
+      var a = new Node({type: 'a'});
+      var z = new Node({type: 'z'});
+
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+
+      parent.pushNode(a);
+      parent.pushNode(foo);
+      parent.pushNode(z);
+
+      assert.equal(bar.next.type, 'baz');
+      assert.equal(baz.next.type, 'z');
+    });
+  });
+
+  describe('.prev', function() {
+    it('should get the prev node from `node.nodes`', function() {
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+
+      assert.equal(bar.prev, null);
+      assert.equal(baz.prev.type, 'bar');
+    });
+
+    it('should get the prev node from `node.parent.nodes`', function() {
+      var parent = new Node({type: 'parent'});
+      var a = new Node({type: 'a'});
+      var z = new Node({type: 'z'});
+
+      var foo = new Node({type: 'foo'});
+      var bar = new Node({type: 'bar'});
+      var baz = new Node({type: 'baz'});
+      foo.pushNode(bar);
+      foo.pushNode(baz);
+
+      parent.pushNode(a);
+      parent.pushNode(foo);
+      parent.pushNode(z);
+
+      assert.equal(bar.prev.type, 'a');
+      assert.equal(baz.prev.type, 'bar');
+    });
+  });
+
+  describe('.getNode', function() {
+    it('should get the node from `node.nodes`', function() {
+      var brace = ast.getNode('brace');
+      assert.equal(brace.type, 'brace');
+      var open = brace.getNode('brace.open');
+      assert.equal(open.type, 'brace.open');
+    });
+
+    it('should get the node from `node.nodes`', function() {
+      var bos = ast.getNode(0);
+      assert.equal(bos.type, 'bos');
+      var brace = ast.getNode(1);
+      assert.equal(brace.type, 'brace');
+    });
+  });
+
+  describe('.pushNode', function() {
+    it('should add a node to `node.nodes`', function() {
+      var node = new Node({type: 'foo'});
+      ast.pushNode(node);
+      assert.equal(ast.last.type, 'foo');
+    });
+
+    it('should set the parent on the given node', function() {
+      var node = new Node({type: 'foo'});
+      ast.pushNode(node);
+      assert(ast === node.parent);
+    });
+
+    it('should set the parent.nodes as `.siblings` on the given node', function() {
+      var node = new Node({type: 'foo'});
+      ast.pushNode(node);
+      assert.equal(node.siblings.length, 4);
+    });
   });
 });
