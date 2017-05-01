89842f66fbb57496451a1f19e0367d0c353df08b
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-01 08:08:05 -0400

    adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var eslint = require('gulp-eslint');
+
+gulp.task('coverage', function() {
+  return gulp.src(['index.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['coverage'], function() {
+  return gulp.src('test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['index.js', 'test/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('default', ['test', 'lint']);
